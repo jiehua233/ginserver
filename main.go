1da4ae60518eb805f328c9e5da95bfa6ab3b181c
jiehua <1753970025@qq.com>
2015-09-07 22:20:29 +0800

    seelograven

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 
+	log "github.com/cihub/seelog"
 	"github.com/getsentry/raven-go"
 	"gopkg.in/yaml.v2"
 )
@@ -12,6 +14,7 @@ var configfile = flag.String("c", "config.yaml", "The config file.")
 var cfg struct {
 	Sentry  string
 	Server  string
+	Logger  string
 	DevMode bool
 }
 var Raven *raven.Client
@@ -33,8 +36,25 @@ func parseCmdLine() {
 			panic(err)
 		}
 	}
+	fmt.Printf("%v\n", cfg)
 }
 
 func initLogger() {
+	var err error
+
+	// 初始化raven
+	if cfg.Sentry != "" {
+		Raven, err = raven.NewClient(cfg.Sentry, nil)
+		if err != nil {
+			log.Error("Init Sentry Error:", err)
+		}
+	}
+
+	// 初始化logger
+	if cfg.Logger != "" {
+		// 自定义一个seelog raven receiver
+		//receiver := &
+
+	}
 
 }
